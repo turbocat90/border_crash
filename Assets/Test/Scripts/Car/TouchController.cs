8666afc8691cf01df10c69262f775565131b71c5
turbocat90 <turbocat90@yandex.ru>
2022-09-27 16:58:27 +0700

    asd

--- Assets/Test/Scripts/Car/TouchController.cs
+++ Assets/Test/Scripts/Car/TouchController.cs
@@ -5,29 +5,47 @@ using UnityEngine;
 public class TouchController : MonoBehaviour
 {
     public float moveDirection;
-    public bool braking;
+    public bool braking { get; set; } = false;
+    public bool isStarting { get; set; } = false;
     private void Update()
     {
-        if (Input.touchCount > 0 && Input.touchCount < 2)
+        if (isStarting)
         {
-            Touch touch = Input.GetTouch(0);
-            if (touch.position.x < 0)
+            if (Input.touchCount > 0 && Input.touchCount < 2)
             {
-                moveDirection = -1;
+                Touch touch = Input.GetTouch(0);
+                if (touch.position.x < Screen.width / 2)
+                {
+                    moveDirection = -1;
+                }
+                else
+                {
+                    moveDirection = 1;
+                }
             }
-            else
+            else if (Input.touchCount == 0)
             {
-                moveDirection = 1;
+                moveDirection = 0;
+            }
+            else if (Input.touchCount >= 2)
+            {
+                moveDirection = 0;
+                if (Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(1).phase == TouchPhase.Began)
+                {
+                    braking = true;
+                }
+                else if (Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(1).phase == TouchPhase.Ended)
+                {
+                    braking = false;
+                }
             }
         }
-        else if (Input.touchCount == 0)
-        {
-            moveDirection = 0;
-        }
-        else if (Input.touchCount >= 2)
-        {
-            moveDirection = 2;
-        }
-
     }
+    IEnumerator StartDelay()
+    {
+        moveDirection = 0;
+        yield return new WaitForSeconds(0.3f);
+        isStarting = true;
+    }
+    public void StartTouch() => StartCoroutine(StartDelay());
 }
